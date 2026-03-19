26a5bb73e9711d015b83623c43a5cb3e90bb218e
Misael-jk <misaelpiucaet12d1@gmail.com>
2026-03-19 09:20:32 -0300

    fix

--- TP Heroes/Test/TestCompania.cs
+++ TP Heroes/Test/TestCompania.cs
@@ -1,53 +1,52 @@
 using Dominio;
 using Dominio.ValueObjects;
 
-namespace Test
+namespace Test;
+
+public class TestCompania
 {
-    public class TestCompania
+    [Fact]
+    public void AgregarEmpleado_DebeAgregarIndividuoYAsignarCompania()
     {
-        [Fact]
-        public void AgregarEmpleado_DebeAgregarIndividuoYAsignarCompania()
-        {
-            // Arrange
-            var nombre = NombreCompania.Create("La Alianza");
-            var compania = Compania.Create(nombre);
-            var individuo = Individuo.Create(NombreIndividuo.Create("Aragorn"), Apodo.Create("El Rey"));
+        // Arrange
+        var nombre = NombreCompania.Create("La Alianza");
+        var compania = Compania.Create(nombre);
+        var individuo = Individuo.Create(NombreIndividuo.Create("Aragorn"), Apodo.Create("El Rey"));
 
-            // Act
-            compania.AgregarEmpleado(individuo);
-            individuo.AsignarCompania(compania);
+        // Act
+        compania.AgregarEmpleado(individuo);
+        individuo.AsignarCompania(compania);
 
-            // Assert
-            Assert.Single(compania.Empleados);
-            Assert.Contains(individuo, compania.Empleados);
-        }
+        // Assert
+        Assert.Single(compania.Empleados);
+        Assert.Contains(individuo, compania.Empleados);
+    }
 
-        [Fact]
-        public void AgregarEmpleado_CuandoYaExiste_DebeLanzarExcepcion()
-        {
-            // Arrange
-            var compania = Compania.Create(NombreCompania.Create("Gondor"));
-            var individuo = Individuo.Create(NombreIndividuo.Create("Aragorn"), Apodo.Create("El Rey"));
-            compania.AgregarEmpleado(individuo);
+    [Fact]
+    public void AgregarEmpleado_CuandoYaExiste_DebeLanzarExcepcion()
+    {
+        // Arrange
+        var compania = Compania.Create(NombreCompania.Create("Gondor"));
+        var individuo = Individuo.Create(NombreIndividuo.Create("Aragorn"), Apodo.Create("El Rey"));
+        compania.AgregarEmpleado(individuo);
 
-            
+        
 
-            var ex = Assert.Throws<Exception>(() => compania.AgregarEmpleado(individuo));
-            Assert.Equal("El empleado ya pertenece a la compañía", ex.Message);
-        }
+        var ex = Assert.Throws<Exception>(() => compania.AgregarEmpleado(individuo));
+        Assert.Equal("El empleado ya pertenece a la compañía", ex.Message);
+    }
 
-        [Fact]
-        public void AgregarArma_Nueva_DebeAparecerEnArmitas()
-        {
-            // Arrange
-            var compania = Compania.Create(NombreCompania.Create("Gondor"));
+    [Fact]
+    public void AgregarArma_Nueva_DebeAparecerEnArmitas()
+    {
+        // Arrange
+        var compania = Compania.Create(NombreCompania.Create("Gondor"));
 
-            // Act
-            compania.AgregarArma("Excalibur", 100);
+        // Act
+        compania.AgregarArma("Excalibur", 100);
 
-            // Assert
-            Assert.NotEmpty(compania.Armitas);
-            Assert.Contains(compania.Armitas, a => a._nombre == "Excalibur");
-        }
+        // Assert
+        Assert.NotEmpty(compania.Armitas);
+        Assert.Contains(compania.Armitas, a => a._nombre == "Excalibur");
     }
 }
